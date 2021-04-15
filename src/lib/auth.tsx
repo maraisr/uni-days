93e7db46bd9a878b6b7b79638cdf793d41c201b0
Marais Rossouw <me@marais.co>
2021-04-16 08:57:24 +1000

    chore: wip on auth

--- src/lib/auth.tsx
+++ src/lib/auth.tsx
@@ -2,8 +2,18 @@ import type { FunctionComponent } from 'react';
 import * as React from 'react';
 import { createContext, useContext } from 'react';
 
+// check https://github.com/auth0/auth0-spa-js/blob/e4b04f3f74fd54e5ed27bc48e01ba03c6655667d/src/cache.ts#L134
+
 interface AuthContext {
 	isAuthenticated: boolean;
+}
+
+interface AuthClient {
+	isAuthenticated(): boolean;
+	getToken(): Promise<string>;
+	login(username: string, password: string): Promise<boolean>;
+	logout(): boolean;
+	register(username: string, password: string): Promise<boolean>;
 }
 
 const context = createContext<AuthContext | null>(null);
