8662b8dce261545a3375062cacc64f1ff8d7f82b
Marais Rossouw <me@marais.co>
2021-04-16 21:23:51 +1000

    chore: amend search to not swap routes

--- src/modules/Search/SearchInput.tsx
+++ src/modules/Search/SearchInput.tsx
@@ -1,12 +1,7 @@
 import { SearchIcon } from '@heroicons/react/outline';
 import type { ChangeEventHandler } from 'react';
 import * as React from 'react';
-import {
-	unstable_startTransition,
-	useCallback,
-	useEffect,
-	useState,
-} from 'react';
+import { unstable_startTransition, useCallback, useRef, useState } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 
 import styles from './SearchInput.module.css';
@@ -15,40 +10,37 @@ export const SearchInput = () => {
 	const navigate = useNavigate();
 	const [params] = useSearchParams();
 	const [loadedSearchTerm] = useState(() => params.get('q') ?? '');
-	const [search, setSearchValue] = useState(loadedSearchTerm);
-
-	useEffect(() => {
-		const tm = setTimeout(() => {
-			const replace = /dashboard$/.test(window.location.pathname);
-
-			unstable_startTransition(() => {
-				if (search) {
-					navigate(
-						{
-							pathname: '/dashboard',
-							search: `?q=${encodeURIComponent(search)}`,
-						},
-						{
-							replace,
-						},
-					);
-				} else {
-					navigate(
-						{
-							pathname: '/dashboard',
-						},
-						{ replace },
-					);
-				}
-			});
-		}, 300);
-
-		return () => void clearTimeout(tm);
-	}, [search]);
+	const changeRef = useRef<number>();
 
 	const onChangeHandler = useCallback<ChangeEventHandler<HTMLInputElement>>(
 		(e) => {
-			setSearchValue(e.currentTarget.value);
+			const value = e.currentTarget.value;
+
+			if (changeRef.current) clearTimeout(changeRef.current);
+			changeRef.current = setTimeout(() => {
+				const replace = /dashboard$/.test(window.location.pathname);
+
+				unstable_startTransition(() => {
+					if (value) {
+						navigate(
+							{
+								pathname: '/dashboard',
+								search: `?q=${encodeURIComponent(value)}`,
+							},
+							{
+								replace,
+							},
+						);
+					} else {
+						navigate(
+							{
+								pathname: '/dashboard',
+							},
+							{ replace },
+						);
+					}
+				});
+			}, 300);
 		},
 		[],
 	);
