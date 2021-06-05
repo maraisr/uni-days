f6ca7e3c782d4c69a45b5f1483bcf2c6d6cd415e
Marais Rossouw <me@marais.co>
2021-06-05 15:52:59 +1000

    chore: added swagger

--- assignment-2/src/app.js
+++ assignment-2/src/app.js
@@ -1,7 +1,8 @@
 import { platform } from 'os';
-import { readdir, lstat } from 'fs/promises';
-import { resolve, relative } from 'path';
+import { lstat, readdir, readFile } from 'fs/promises';
+import { relative, resolve } from 'path';
 import { async_handler } from './helpers/async_handler.js';
+import swaggerUI from 'swagger-ui-express';
 
 /**
  * @typedef {import("@types/express").Application} ExpressApplication
@@ -12,14 +13,21 @@ import { async_handler } from './helpers/async_handler.js';
  * @param {ExpressApplication} app
  */
 export const bootstrap = async (app) => {
+	const doc = JSON.parse(await readFile('./data/swagger.json', 'utf8'));
+
+	app.use(swaggerUI.serve);
+	app.get('/', swaggerUI.setup(doc));
+
 	const routes = new Map();
 
 	await visit_files('src/routes', (rel, abs) => {
 		if (!/\.[tj]sx?$/.test(rel)) return;
-		const name = rel
+		let name = rel
 			.replace(/\.[tj]sx?$/, '')
 			.replaceAll(/[\\/]+/g, '/')
 			.replace(/(\/|^)\$/, '$1:');
+
+		if (name === 'index') name = '';
 
 		if (platform() === 'win32') routes.set(name, import(`file://${abs}`));
 		else routes.set(name, import(abs));
@@ -34,6 +42,7 @@ export const bootstrap = async (app) => {
 			const mapped = (Array.isArray(handle) ? handle : [handle]).map(
 				async_handler,
 			);
+
 			app[verb](`/${route}`, ...mapped);
 		}
 	}
@@ -44,7 +53,8 @@ const visit_files = async (directory, visitor, base_directory = directory) => {
 		const file = resolve(directory, filename);
 		const stat = await lstat(file);
 
-		if (stat.isDirectory()) visit_files(file, visitor, base_directory);
+		if (stat.isDirectory())
+			await visit_files(file, visitor, base_directory);
 		else if (stat.isFile()) visitor(relative(base_directory, file), file);
 	}
 };
